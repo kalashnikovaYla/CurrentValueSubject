9c62bae30650a1afc6cf5320f01ccd15f16f3a5e
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-05-11 21:25:00 +0500

    Initial Commit

---
+++ CurrentValueSubject/CurrentValueSubjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrentValueSubjectApp.swift
+//  CurrentValueSubject
+//
+//  Created by sss on 11.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrentValueSubjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
